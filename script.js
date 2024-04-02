b7ba3728b88ff6582bdb1042513626c80dd0b8bd
Lukas Jurak <jur42282@spseol.cz>
2024-04-02 09:27:25 +0200

    console in html

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-alert("Welcome to summoner's rift!")
+var native = window;
+var originalConsole = native.console;
+native.console = {
+    log: function(message){
+        $('ul.messages').append('<li>Log: ' + message + '</li>');
+        originalConsole.log(message);
+    },
+    error: function(message){
+        $('ul.messages').append('<li>Error: ' + message + '</li>');
+        originalConsole.error(message);
+    },
+    warn: function(message){
+        $('ul.messages').append('<li>Warn: ' + message + '</li>');
+        originalConsole.warn(message);
+    }
+}
+
+console.log("hfghf")
+
+let msg = prompt("Enter your message");
+console.log(msg);
